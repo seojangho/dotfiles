f73b6be70aad325b732a807d7b8e8020c6d1fda2
JangHo Seo <jangho@jangho.kr>
2015-08-02 19:05:16 +0900

    add shebang line to ./apps/vim/install.sh

--- apps/vim/install.sh
+++ apps/vim/install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 ln -s ~/.jangho/apps/vim/vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugUpdate +qall
