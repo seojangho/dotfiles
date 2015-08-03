288fb27e74b20c513d3960870b68fb9f84d65971
JangHo Seo <jangho@jangho.kr>
2015-08-04 03:34:55 +0900

    clone script ran by root can make backup unreadable by others

--- hosts/mango/clone.sh
+++ hosts/mango/clone.sh
@@ -7,7 +7,9 @@ echo `date +%Y-%m-%d--%H-%M-%S--%Z` >> $path_backup_logfile
 
 cd /mnt/mango-HDD/Clone/mango
 
-rsync -rltDh --delete /home/jangho/public_html ./ 2>>$path_backup_logfile
-rsync -rltDh --delete /home/jangho/jangho.git ./ 2>>$path_backup_logfile
-rsync -rltDh --delete /home/jangho/jangho-secure.git ./ 2>>$path_backup_logfile
-rsync -rltDh --delete /home/jangho/jangho-wiki.git ./ 2>>$path_backup_logfile
+rsync -rltDh --delete --chmod=ugo=rwX /home/jangho/public_html ./ 2>>$path_backup_logfile
+rsync -rltDh --delete --chmod=ugo=rwX /home/jangho/jangho.git ./ 2>>$path_backup_logfile
+rsync -rltDh --delete --chmod=ugo=rwX /home/jangho/jangho-secure.git ./ 2>>$path_backup_logfile
+rsync -rltDh --delete --chmod=ugo=rwX /home/jangho/jangho-wiki.git ./ 2>>$path_backup_logfile
+rsync -rltDh --delete --chmod=ugo=rwX /var/lib/taskd/orgs ./ 2>>$path_backup_logfile
+rsync -rltDh --delete --chmod=ugo=rwX /var/lib/znc/.znc/moddata/log ./ 2>>$path_backup_logfile
