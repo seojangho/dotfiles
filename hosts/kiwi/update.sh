b33f87550710dfb1a4e9dd1f397ff092c4b7681d
JangHo Seo <jangho@jangho.kr>
2015-09-26 16:11:11 +0900

    read as shell command

--- hosts/kiwi/update.sh
+++ hosts/kiwi/update.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 echo "update gitit by stack"
 read -n1
--- hosts/lime/update.sh
+++ hosts/lime/update.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 echo "update Firefox theme from https://github.com/horst3180/arc-firefox-theme/releases"
 read -n1
